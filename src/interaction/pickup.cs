fe95bad092cbf9c02bc4a9fe631d594dffb04372
Hank Morgan <aidanontour@yahoo.co.uk>
2024-02-24 17:08:17 +0000

    object quantities can now be selected/split from the game world

--- src/interaction/pickup.cs
+++ src/interaction/pickup.cs
@@ -1,4 +1,7 @@
 using Godot;
+using Peaky.Coroutines;
+using System.Collections;
+
 namespace Underworld
 {
     /// <summary>
@@ -8,10 +11,10 @@ namespace Underworld
     {
         public static bool Drop(int index, uwObject[] objList, Vector3 dropPosition, int tileX, int tileY)
         {
-            var t = UWTileMap.current_tilemap.Tiles[tileX,tileY];
+            var t = UWTileMap.current_tilemap.Tiles[tileX, tileY];
             if (t.tileType == UWTileMap.TILE_SOLID)
             {
-                uimanager.AddToMessageScroll(GameStrings.GetString(1,GameStrings.str_there_is_no_place_to_put_that_));
+                uimanager.AddToMessageScroll(GameStrings.GetString(1, GameStrings.str_there_is_no_place_to_put_that_));
                 return false;
             }
             else
@@ -19,27 +22,27 @@ namespace Underworld
                 //translate drop position into xpos, ypos and zpos on the tile.
                 //for the moment just use 4,4 and floorheight.
                 var obj = objList[index];
-                obj.xpos= uwObject.FloatXYToXYPos(-dropPosition.X); 
-                obj.ypos= uwObject.FloatXYToXYPos(dropPosition.Z);                
+                obj.xpos = uwObject.FloatXYToXYPos(-dropPosition.X);
+                obj.ypos = uwObject.FloatXYToXYPos(dropPosition.Z);
                 obj.zpos = uwObject.FloatZToZPos(dropPosition.Y); //(short)(t.floorHeight<<2);
-                
-                obj.tileX= tileX; obj.tileY = tileY;
+
+                obj.tileX = tileX; obj.tileY = tileY;
                 obj.next = t.indexObjectList;
                 t.indexObjectList = (short)index;
                 //remove the old
                 // why was this here 
                 //ObjectCreator.RemoveObject(obj);  
                 //create the new              
-                ObjectCreator.RenderObject(obj,UWTileMap.current_tilemap);
+                ObjectCreator.RenderObject(obj, UWTileMap.current_tilemap);
                 return true;
             }
         }
 
         static bool CanBePickedUpOverrides(int item_id)
         {
-            if (_RES!=GAME_UW2)
+            if (_RES != GAME_UW2)
             {
-                if (item_id==458)
+                if (item_id == 458)
                 {
                     return true;
                 }
@@ -62,50 +65,125 @@ namespace Underworld
                     uimanager.AddToMessageScroll(GameStrings.GetString(1, GameStrings.str_you_cannot_pick_that_up_));
                     return false;
                 }
-                //player is trying to pick something up
-                playerdat.ObjectInHand = index;
-                uimanager.instance.mousecursor.SetCursorArt(obj.item_id);
-
-                //remove from it's tile
-                var tile = UWTileMap.current_tilemap.Tiles[obj.tileX, obj.tileY];
-                int nextObjectIndex = tile.indexObjectList;
-                if (nextObjectIndex==index)
-                {//object is first in list, easy swap
-                    tile.indexObjectList = obj.next;
+                if (obj.ObjectQuantity > 1)
+                {
+                    //prompt for quantity in coroutine.
+                    _ = Peaky.Coroutines.Coroutine.Run(
+                            DoPickupQty(index, objList, obj),
+                            main.instance
+                        );
+                    return true;
                 }
                 else
                 {
-                    while(nextObjectIndex!=0)
-                    {
-                        var nextObj = objList[nextObjectIndex];
-                        if (nextObj.next==index)
-                        {
-                            nextObj.next = obj.next;
-                            nextObjectIndex = 0;
-                        }
-                        else
-                        {
-                            nextObjectIndex = nextObj.next;
-                        }                       
-                    }
-                } 
-                obj.next = 0; //ensure end of chain.               
-                obj.tileX=99;obj.tileY=99;
-                if (obj.instance!=null)
-                {
-                    obj.instance.uwnode.Position = obj.GetCoordinate(obj.tileX, obj.tileY);
-                }
-
-                //now handle some special cases
-                if (_RES!=GAME_UW2)
-                {
-                    if (obj.item_id==458)//silver tree
-                    {
-                        silvertree.PickupTree(obj);
-                    }
+                    //single instance object
+                    DoPickup(index, objList, obj);
                 }
             }
             return true;
-        }    
+        }
+
+        /// <summary>
+        /// Handles pickup up stacks of objects which need to be split
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="objList"></param>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static IEnumerator DoPickupQty(int index, uwObject[] objList, uwObject obj)
+        {
+            MessageDisplay.WaitingForTypedInput = true;
+            
+            uimanager.instance.TypedInput.Text = "";
+            uimanager.instance.scroll.Clear();
+            uimanager.AddToMessageScroll("Move how many? {TYPEDINPUT}|");
+
+            while (MessageDisplay.WaitingForTypedInput)
+            {
+                yield return new WaitOneFrame();
+            }
+            
+            var response = uimanager.instance.TypedInput.Text;
+            if (int.TryParse(response, out int result))
+            {
+                if (obj.ObjectQuantity <= result)
+                {//at least all of the stack is seleced
+                    DoPickup(index, objList, obj);
+                }
+                else
+                {
+                    //if <quantity selected, split objects, pickup object of that quantity.
+                    var newObjIndex = ObjectCreator.SpawnObjectInHand(obj.item_id); //spawning in hand is very handy here
+                    var newObj = UWTileMap.current_tilemap.LevelObjects[newObjIndex];
+                    newObj.link = (short)result;
+                    newObj.quality = obj.quality;
+                    newObj.owner= obj.owner;
+                    //TODO. see if other object properties need copying.                    
+                    obj.link = (short)(obj.link - result);//reduce the other object.
+                }
+                yield return true;
+            }
+            else
+            {
+                //invalid input. cancel
+                yield return false;
+            }
+            
+            
+
+            //full quantity select, pickup all
+
+            //if 0, cancel.
+            yield return false;
+        }
+
+        private static void DoPickup(int index, uwObject[] objList, uwObject obj)
+        {
+            //player is trying to pick something up
+            playerdat.ObjectInHand = index;
+            uimanager.instance.mousecursor.SetCursorArt(obj.item_id);
+
+            //remove from it's tile
+            var tile = UWTileMap.current_tilemap.Tiles[obj.tileX, obj.tileY];
+            int nextObjectIndex = tile.indexObjectList;
+            if (nextObjectIndex == index)
+            {//object is first in list, easy swap
+                tile.indexObjectList = obj.next;
+            }
+            else
+            {
+                while (nextObjectIndex != 0)
+                {
+                    var nextObj = objList[nextObjectIndex];
+                    if (nextObj.next == index)
+                    {
+                        nextObj.next = obj.next;
+                        nextObjectIndex = 0;
+                    }
+                    else
+                    {
+                        nextObjectIndex = nextObj.next;
+                    }
+                }
+            }
+            obj.next = 0; //ensure end of chain.               
+            obj.tileX = 99; obj.tileY = 99;
+            if (obj.instance != null)
+            {
+                obj.instance.uwnode.Position = obj.GetCoordinate(obj.tileX, obj.tileY);
+            }
+
+            //now handle some special cases
+            if (_RES != GAME_UW2)
+            {
+                if (obj.item_id == 458)//silver tree
+                {
+                    silvertree.PickupTree(obj);
+                }
+            }
+        }
+    
+    
+
     } //end class
 }//end namespace
