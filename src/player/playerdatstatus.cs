88481263986b9acf0656bae78353c4f49af10ce8
Hank Morgan <aidanontour@yahoo.co.uk>
2024-02-29 10:05:26 +0000

    Setter for ActiveSpellEffectCount

--- src/player/playerdatstatus.cs
+++ src/player/playerdatstatus.cs
@@ -22,6 +22,24 @@ namespace Underworld
                          return (GetAt16(0x60)>>6) & 0xF;
                     }                    
                }
+
+               set
+               {
+                    if (_RES==GAME_UW2)
+                    {
+                        var temp = GetAt16(0x61);
+                        temp &= 0xFE1F; //clear bits
+                        temp |= (value <<5); //set new value
+                        SetAt16(0x61, temp);
+                    }
+                    else
+                    {
+                         var temp = GetAt16(0x60);
+                        temp &= 0xFC3F; //clear bits
+                        temp |= (value <<6); //set new value
+                        SetAt16(0x61, temp);
+                    }  
+               }
           }
 
           public static int SilverTreeLevel
