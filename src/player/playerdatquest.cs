0aed46290b77f850b5218391bea2da936087a563
Hank Morgan <aidanontour@yahoo.co.uk>
2024-04-03 23:02:03 +0100

    listed known quest variables

--- src/player/playerdatquest.cs
+++ src/player/playerdatquest.cs
@@ -204,3 +204,118 @@ namespace Underworld
         }
     }//end class
 }//end namespace
+
+//quests are
+/// 7.8.1 UW1 quest flags
+/// 
+/// flag   description
+/// 
+/// 0    Dr. Owl's assistant Murgo freed
+/// 1    talked to Hagbard
+/// 2    met Dr. Owl?
+/// 3    permission to speak to king Ketchaval
+/// 4    Goldthirst's quest to kill the gazer (1: gazer killed)
+/// 5    Garamon, find talismans and throw into lava
+/// 6    friend of Lizardman folk
+/// 7    ?? (conv #24, Murgo)
+/// 8    book from Bronus for Morlock
+/// 9    "find Gurstang" quest
+/// 10    where to find Zak, for Delanrey
+/// 11    Rodrick killed
+/// 
+/// 32    status of "Knight of the Crux" quest
+/// 0: no knight
+/// 1: seek out Dorna Ironfist
+/// 2: started quest to search the "writ of Lorne"
+/// 3: found writ
+/// 4: door to armoury opened
+/// 
+/// 36: No of talismans still to destroy.
+/// 37: Garamon dream stages
+/// 
+/// UW2 Quest flags
+/// 0: PT related - The troll is released
+/// 1: PT related - Bishop is free?
+/// 2: PT related - Borne wants you to kill bishop
+/// 3: PT related - Bishop is dead
+/// 
+/// 6: Helena asks you to speak to praetor loth
+/// 7: Loth is dead
+/// 8:  Kintara tells you about Javra
+/// 9:  Lobar tells you about the "virtues"
+/// 10: PT related
+/// 11: Listener under the castle is dead.
+/// 12: used in Ice caverns to say the avatar can banish the guardians presence. Wand of altara?
+/// 13: Mystral wants you to spy on altara
+/// 14: Likely all lines of power have been cut.
+/// 15: Altara tells you about the listener
+/// 
+/// 18: You learn that the Trikili can talk.
+/// 19: You know Relk has found the black jewel (josie tells you)
+/// 20: You've met Mokpo
+/// 22: Blog is now your friend(?)
+/// 23: You have used Blog to defeat dorstag
+/// 24: You have won a duel in the arena
+/// 25: You have defeated Zaria in the pits
+/// 26: You know about the magic scepter (for the Zoranthus)
+/// 27: You know about the sigil of binding/got the djinn bottle(by bringing the scepter to zorantus)
+/// 28: Took Krilner as a slave
+/// 29: You know Dorstag has the gem(?)
+/// 30: Dorstag refused your challenge(?)
+/// 32: Met a Talorid
+/// 33: You have agreed to help the talorid (historian)
+/// 34: Met or heard of Eloemosynathor
+/// 35: The vortz are attacking!
+/// 36: Quest to clarify question for Data Integrator
+/// 37: talorus related (checked by futurian)
+/// 38: talorus related *bliy scup is regenerated
+/// 39: talorus related
+/// 40: Dialogian has helped with data integrator
+/// 43: Patterson has gone postal
+/// 45: Janar has been met and befriended
+/// 
+/// 47: You have recieve the quest from the triliki
+/// 48: You have dreamed about the void
+/// 49: Bishop tells you about the gem.
+/// 50: The keep is going to crash.
+/// 51: You have visited the ice caves (britannia becomes icy)
+/// 52: Have you cut the line of power in the ice caverns
+/// 54: Checked by Mors Gotha? related to keep crashing
+/// 55: Banner of Killorn returned (based on Scd.ark research)
+/// 58: Set when meeting bishop. Bishop tells you about altara
+/// 60: Possible prison tower variable. used in check trap on second highest level
+/// 61: You've brought water to Josie
+/// 
+/// 63: Blog has given you the gem
+/// 64: Is mors dead
+/// 65: Pits related (checked by dorstag)
+/// 68: You have given the answers to nystrul and the invasion (endgame) has begun.
+/// 104: Set when you enter scintilus level 5 (set by variable trap)
+/// 105: Set when the air daemon is absorbed. (see also xclock1 and xclock3 changes)
+/// 106: Got or read mors spellbook
+/// 107: Set after freeing praetor loth and you/others now know about the horn.
+/// 109: Set to 1 after first LB conversation. All castle occupants check this on first talk.
+/// 110: Checked when talking to LB and Dupre. The guardians forces are attacking
+/// 112: checked when talking to LB. You have been fighting with the others
+/// 114: checked when talking to LB. The servants are restless
+/// 115: checked when talking to LB. The servants are on strike
+/// 116: The strike has been called off.
+/// 117: Mors has been defeated in Kilorn
+/// 118: The wisps tell you about the trilikisssss2
+/// 119: Fizzit the thief surrenders
+/// 120: checked by miranda?
+/// 121: You have defeated Dorstag
+/// 122: You have killed the bly scup ductosnore
+/// 123: Relk is dead
+/// 124 & 126 are referenced in teleport_trap
+/// 128: 0-128 bit field of where the lines of power have been broken.
+/// 129: How many enemies killed in the pits (also xclock 14)
+/// 131: You are told that you are in the prison tower =1  
+/// 	You are told you are in kilhorn keep =3
+/// 	You are told you are in scintilus = 19
+/// 	(this is probably a bit field.)
+/// 132: Set to 2 during Kintara conversation
+/// 133: How much Jospur owes you for fighting in the pits
+/// 134: The password for the prison tower (random value)
+/// 135: Checked by goblin in sewers  (no of worms killed on level. At more than 8 they give you fish)
+/// 143: Set to 33 after first LB conversation. Set to 3 during endgame (is this what triggers the cutscenes?)
