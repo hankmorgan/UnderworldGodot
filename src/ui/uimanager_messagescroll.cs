658fd01931085b749c0e76870961030610f042fd
Hank Morgan <aidanontour@yahoo.co.uk>
2025-01-27 21:04:55 +0000

    tweaked message display column width for uw2

--- src/ui/uimanager_messagescroll.cs
+++ src/ui/uimanager_messagescroll.cs
@@ -43,7 +43,7 @@ namespace Underworld
 				instance.convo.Lines = new MessageScrollLine[12];
 				instance.convo.Rows = 12;
 				instance.convo.Columns = 40;
-				instance.scroll.Columns = 50;
+				instance.scroll.Columns = 44;
 			}
 			else
 			{
