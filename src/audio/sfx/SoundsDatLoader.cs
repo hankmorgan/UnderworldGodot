41cda23091560cab3fd7353ba708bc1f52e02647
Hank Morgan <aidanontour@yahoo.co.uk>
2026-04-17 20:18:36 +0100

    SoundsDatLoader now supports UW2 block size of 8 for sounds.dat entries

--- src/audio/sfx/SoundsDatLoader.cs
+++ src/audio/sfx/SoundsDatLoader.cs
@@ -4,6 +4,20 @@ namespace Underworld.Sfx;
 
 public static class SoundsDatLoader
 {
+    static int BlockSize
+    {
+        get
+        {
+            if (UWClass._RES == UWClass.GAME_UW2)
+            {
+                return 8;
+            }
+            else
+            {
+                return 5;
+            }
+        }
+    }
     public static SoundEntry[] Load(string path)
     {
         var data = File.ReadAllBytes(path);
@@ -11,7 +25,7 @@ public static class SoundsDatLoader
             throw new InvalidDataException("SOUNDS.DAT is empty");
 
         int count = data[0];
-        int need = 1 + count * 5;
+        int need = 1 + count * BlockSize;
         if (data.Length < need)
             throw new InvalidDataException(
                 $"SOUNDS.DAT truncated: need {need} bytes for {count} entries, got {data.Length}");
@@ -19,7 +33,7 @@ public static class SoundsDatLoader
         var entries = new SoundEntry[count];
         for (int i = 0; i < count; i++)
         {
-            int o = 1 + i * 5;
+            int o = 1 + i * BlockSize;
             entries[i] = new SoundEntry(
                 PatchNum:     data[o + 0],
                 Note:         data[o + 1],
