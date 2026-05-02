8f3775a2e4f06c207295eb3205e57d0fe6db6915
Hank Morgan <aidanontour@yahoo.co.uk>
2026-05-02 17:17:07 +0100

    reverted some automapping rules from previous commit. updated logic of tilediscovery to take into account tile type 11 (undiscovered tile marker)

--- src/World/automap.cs
+++ src/World/automap.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 
 namespace Underworld
@@ -58,12 +59,32 @@ namespace Underworld
 
         public static void MarkTileVisited(int level, int tileX, int tileY, int tiletype, int displaytype = automaptileinfo.DisplayTypeClear)
         {
-            if ((tiletype != UWTileMap.TILE_SOLID) && (automaps[level].tiles[tileX, tileY].tileType == UWTileMap.TILE_SOLID))
+            var tileToChange = automaps[level].tiles[tileX, tileY];
+            // if (
+            //     (tiletype != UWTileMap.TILE_SOLID) 
+            //     && 
+            //     ((automaps[level].tiles[tileX, tileY].tileType == UWTileMap.TILE_SOLID) || (automaps[level].tiles[tileX, tileY].tileType == 11))
+            //     )
+            // {
+            //     if (tiletype != 11)
+            //     {
+            //         playerdat.NoOfTilesDiscovered++;//for exp gain on discovering new tiles. Skipping over solid tiles and undiscovered.
+            //     }                
+            // }    
+
+            if ((tileToChange.tileType == UWTileMap.TILE_SOLID) || (tileToChange.tileType == 11))
             {
-                playerdat.NoOfTilesDiscovered++;//for exp gain on discovering new tiles. Skipping over solid tiles.
-            }            
-            automaps[level].tiles[tileX, tileY].tileType = (short)tiletype;
-            automaps[level].tiles[tileX, tileY].DisplayType = (short)displaytype;
+                //tile was undiscovered.
+                if ((tiletype != UWTileMap.TILE_SOLID) && (tiletype!=11))
+                {
+                    //and not changing it to an undiscovered or solid type.
+                    playerdat.NoOfTilesDiscovered++; //TODO: this may need to be updated to allow for marking of solid tiles. how are these tracked as been discovered? need to do some file research...
+                }
+            }
+
+
+            tileToChange.tileType = (short)tiletype;
+            tileToChange.DisplayType = (short)displaytype;
         }
 
 
@@ -119,27 +140,27 @@ namespace Underworld
                             tiletype: UWTileMap.current_tilemap.Tiles[x, y].tileType,
                             displaytype: automaptileinfo.GetDisplayType(UWTileMap.current_tilemap.Tiles[x, y]));
                     }
-                    // else
-                    // {
-                    //     if (UWTileMap.ValidTile(x, y))
-                    //     {//mark the open tiles outside of vision range as undiscovered open tile if not already visited
-                    //         var tile = UWTileMap.current_tilemap.Tiles[x, y];
-                    //         if (automaps[dungeon_level - 1].tiles[x, y].visited == false)
-                    //         {
-                    //             if (UWTileMap.IsOpen(UWTileMap.current_tilemap.Tiles[x, y].tileType))
-                    //             {
-                    //                 var displaytype = automaptileinfo.GetDisplayType(tile);
-                    //                 //automap.automaps[dungeon_level - 1].tiles[northaxis, aY].DisplayType;
-                    //                 //mark as undiscovered, open tile. 
-                    //                 MarkTileVisited(
-                    //                     level: dungeon_level - 1,
-                    //                     tileX: x, tileY: y,
-                    //                     tiletype: 11,
-                    //                     displaytype: displaytype);
-                    //             }
-                    //         }
-                    //     }
-                    // }
+                    else
+                    {
+                        if (UWTileMap.ValidTile(x, y))
+                        {//mark the open tiles outside of vision range as undiscovered open tile if not already visited
+                            var tile = UWTileMap.current_tilemap.Tiles[x, y];
+                            if (automaps[dungeon_level - 1].tiles[x, y].visited == false)
+                            {
+                                if (UWTileMap.IsOpen(UWTileMap.current_tilemap.Tiles[x, y].tileType))
+                                {
+                                    var displaytype = automaptileinfo.GetDisplayType(tile);
+                                    //automap.automaps[dungeon_level - 1].tiles[northaxis, aY].DisplayType;
+                                    //mark as undiscovered, open tile. 
+                                    MarkTileVisited(
+                                        level: dungeon_level - 1,
+                                        tileX: x, tileY: y,
+                                        tiletype: 11,
+                                        displaytype: displaytype);
+                                }
+                            }
+                        }
+                    }
                 }
             }
             return range;
